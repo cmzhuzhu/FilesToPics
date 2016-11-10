6568d1707c7362e9294c19c3dba972f4e0129a6c
CaoMingzhu <820741775@qq.com>
2016-11-10 17:55:07 +0800

    Create tests.py

---
+++ tests/tests.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import unittest
+from FileToPics import FileToPics
+
+class TestSimple(unittest.TestCase):
+    def test_nonFilePathParameter(self):
+        file2pics     = FileToPics(picpath = '/home/test/', filetype = 'pdf')
+        resData       = file2pics.file_to_pics()
+        self.assertEqual(resData, {'Code': 400, 'Message': 'the necessary params is not given'})
+
+    def test_pdfToPics(self):
+        file2pics     = FileToPics(filepath = '/home/test.pdf', picpath = '/home/test/', filetype = 'pdf')
+        resData       = file2pics.file_to_pics()
+
+        self.assertNotEqual(resData, None)
+
+    def test_fileToPics(self):
+        file2pics     = FileToPics(filepath = '/home/test.pptx', picpath = '/home/test/', filetype = 'pdf')
+        resData       = file2pics.file_to_pics()
+
+        self.assertNotEqual(resData, None)
+
+
+if __name__ == '__main__':
+    unittest.main()
